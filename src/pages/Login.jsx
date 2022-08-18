f71ab26e22d8799046443039d8d2c74c6eed9f49
Chris Onsando <chrisnemwel@gmail.com>
2022-08-18 15:04:38 +0300

    chore(routes): setup react router (#4)
    * install and configure react router
     * create a folder structure
     * add login routes
     * test login routes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../layout/NavBar';
+
+function Login() {
+  return (
+    <div className="login">
+      <Navbar />
+      <h1>Login to the application</h1>
+    </div>
+  );
+}
+
+export default Login;
