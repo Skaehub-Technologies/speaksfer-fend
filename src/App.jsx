f62a12c24a3c520f43f243195fe36c5fd5e1c1c9
dalienst <oduordalienst@gmail.com>
2022-08-25 14:10:06 +0300

    feature(183031478): user should be able to register
    *user should be able to create a new account
    
    *connect backend endpoint to frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BaseRouter from './constants/Routes';
+import './assets/styles/main.scss';
 
 function App() {
   return (
--- src/constants/Routes.jsx
+++ src/constants/Routes.jsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import publicLinks from './links';
+import { publicLinks } from './links';
 
 const Home = React.lazy(() => import('../pages/Home'));
 const Login = React.lazy(() => import('../pages/Login'));
+const Registration = React.lazy(() => import('../pages/Registration'));
 
 function BaseRouter() {
   return (
@@ -12,6 +13,7 @@ function BaseRouter() {
         <Routes>
           <Route exact path={publicLinks.home} element={<Home />} />
           <Route path={publicLinks.Login} element={<Login />} />
+          <Route path={publicLinks.Registration} element={<Registration />} />
         </Routes>
       </Suspense>
     </Router>
